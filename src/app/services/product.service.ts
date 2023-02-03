d4704d05b8a6a0160bd646008f3216a3662989d2
Mehmet AKAR <190601001@bakircay.edu.tr>
2023-02-03 21:29:19 +0300

    finalProject frontend init

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../Models/listResponseModel';
+import { Product } from '../Models/product';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl = 'https://localhost:44348/api/';
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts():Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl+"products/getall";
+   return  this.httpClient
+      .get<ListResponseModel<Product>>(newPath);
+  }
+
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl+"products/getbycategory?categoryId="+categoryId;
+    return  this.httpClient
+       .get<ListResponseModel<Product>>(newPath);
+   }
+}
